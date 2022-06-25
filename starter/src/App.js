836ae6ad9e3a944ba1d4bdd2acab18fc9f418c4c
UMAR <umarmushtaq101@gmail.com>
2022-06-25 13:11:00 +0500

    local data state done

--- starter/src/App.js
+++ starter/src/App.js
@@ -3,9 +3,11 @@ import { useEffect } from "react";
 import CartContainer from "./components/CartContainer";
 import { useSelector, useDispatch } from "react-redux";
 import { calculateTotals } from "./features/cart/cartSlice";
+import Modal from "./components/Modal";
 
 function App() {
   const { cartItems } = useSelector((store) => store.cart);
+  const { isOpen } = useSelector((store) => store.modal);
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -14,6 +16,8 @@ function App() {
 
   return (
     <main>
+      {isOpen && <Modal />}
+
       <Navbar />
       <CartContainer />
     </main>
